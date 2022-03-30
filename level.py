501e51a59b0ed7cccff43a2fddf61bad18b64560
Thomas Phan <47121602+tummeito@users.noreply.github.com>
2022-03-30 16:52:50 -0700

    Add files via upload

--- level.py
+++ level.py
@@ -19,14 +19,71 @@ class Level:
                 x = col_index * self.settings.tile_size
                 y = row_index * self.settings.tile_size
                 if cell == 'X':
-                    tile = Tile((x,y),self.settings.tile_size)
+                    tile = Tile((x,y), self.settings.tile_size, 'grey')
                     self.tiles.add(tile)
                 if cell == 'P':
                     player = Player((x,y))
                     self.player.add(player)
+                if cell == 'p':
+                    tile = Tile((x,y), self.settings.tile_size, 'light green')
+                    self.tiles.add(tile)
+                if cell == 'H':
+                    tile = Tile((x, y), self.settings.tile_size, 'green')
+                    self.tiles.add(tile)
+                if cell == 'B':
+                    tile = Tile((x, y), self.settings.tile_size, 'brown')
+                    self.tiles.add(tile)
+                if cell == '?':
+                    tile = Tile((x, y), self.settings.tile_size, 'orange')
+                    self.tiles.add(tile)
+
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.vector.x
+
+        if player_x < self.settings.screen_width / 4 and direction_x < 0:
+            self.world_shift = self.settings.player_speed
+            player.speed = 0
+        elif player_x > self.settings.screen_width * 3/4 and direction_x > 0:
+            self.world_shift = -self.settings.player_speed
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = self.settings.player_speed
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.vector.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.vector.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.vector.x > 0:
+                    player.rect.right = sprite.rect.left
+
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.apply_gravity()
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.vector.y > 0:
+                    player.rect.bottom = sprite.rect.top
+                    player.vector.y = 0
+                elif player.vector.y < 0:
+                    player.rect.top = sprite.rect.bottom
+                    player.vector.y = 0
+
 
     def draw(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
+
         self.player.update()
+        self.vertical_movement_collision()
+        self.horizontal_movement_collision()
         self.player.draw(self.display_surface)
--- player.py
+++ player.py
@@ -12,17 +12,30 @@ class Player(Sprite):
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
         self.vector = pg.math.Vector2(0,0)
+        self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pg.key.get_pressed()
 
-        if keys[pg.K_RIGHT or pg.K_d]:
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
             self.vector.x = 1
-        elif keys[pg.K_LEFT or pg.K_a]:
+        elif keys[pg.K_LEFT] or keys[pg.K_a]:
             self.vector.x = -1
         else:
             self.vector.x = 0
 
+        if keys[pg.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.vector.y += self.gravity
+        self.rect.y += self.vector.y
+
+    def jump(self):
+        self.vector.y = self.jump_speed
+
+
     def update(self):
         self.get_input()
-        self.rect.x += self.vector.x * self.settings.player_speed
